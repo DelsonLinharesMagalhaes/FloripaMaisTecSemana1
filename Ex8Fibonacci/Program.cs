cd4f89dc067bf7d3dfa21a4d0c2347a905a3b329
Delson Magalhaes <delson_magalhaes@estudante.sesisenai.org.br>
2023-02-12 21:32:47 -0300

    apagando pasta

--- Ex8Fibonacci/Program.cs
+++ Ex8Fibonacci/Program.cs
@@ -10,36 +10,3 @@ Console.WriteLine("Sequência de Fibonacci");
 Console.WriteLine();
 
 // Declarando váriaveis
-
-int vQtdNum = 0;
-
-// Entrando com o dados
-
-Console.Write("Quantida de números? ");
-vQtdNum = int.Parse(Console.ReadLine());
-
-int[] vSeqFib = new int[vQtdNum];
-
-// Calculando a média da nota do aluno
-
-for (int i = 0; i <=  (vQtdNotas-1); i++){
-    Console.Write("Digite o valor da nota "+(i+1)+": ");
-    vNotas[i] = int.Parse(Console.ReadLine());
-    vSomaMedia+=vNotas[i];
-}
-
-
-vMedia = vSomaMedia/vQtdNotas;
-
-// Exibindo a nota média
-
-Console.WriteLine("");
-Console.WriteLine("A nota média do aluno é: " + vMedia);
-
-if (vMedia >= 6){
-    Console.WriteLine("O aluno foi aprovado!");                 // Maior ou igual a 6 o aluno está aprovado
-} else if(vMedia >= 5){
-    Console.WriteLine("O aluno está de recuperação!");          // Menor que  6 e maior ou igual a 5 o aluno está aprovado
-}else{
-    Console.WriteLine("O aluno foi reprovado!");                // Menor do que 5 o aluno está reprovado
-}
